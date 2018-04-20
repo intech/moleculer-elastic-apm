d5033180a3afc4a99ba9a598f1a00f86dd4d8ae1
Ivan <ru31337@gmail.com>
2018-04-20 12:04:40 +0300

    first

---
+++ README.md
@@ -0,0 +1,34 @@
+# moleculer-elastic-apm
+###### Metrics to Elastic APM Agent
+
+[![NPM Version][npm-image]][npm-url]
+[![NPM Downloads][downloads-image]][downloads-url]
+[![Linux Build][travis-image]][travis-url]
+[![Windows Build][appveyor-image]][appveyor-url]
+[![Test Coverage][coveralls-image]][coveralls-url]
+
+## Install
+
+This is a [Node.js](https://nodejs.org/en/) module available through the
+[npm registry](https://www.npmjs.com/). Installation is done using the
+[`npm install` command](https://docs.npmjs.com/getting-started/installing-npm-packages-locally):
+
+```sh
+$ npm install moleculer-elastic-apm
+```
+
+
+## License
+
+[MIT](LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/moleculer-elastic-apm.svg
+[npm-url]: https://npmjs.org/package/moleculer-elastic-apm
+[travis-image]: https://img.shields.io/travis/intech/moleculer-elastic-apm/master.svg?label=linux
+[travis-url]: https://travis-ci.org/intech/moleculer-elastic-apm
+[appveyor-image]: https://img.shields.io/appveyor/ci/intech/moleculer-elastic-apm/master.svg?label=windows
+[appveyor-url]: https://ci.appveyor.com/project/intech/moleculer-elastic-apm
+[coveralls-image]: https://img.shields.io/coveralls/intech/moleculer-elastic-apm/master.svg
+[coveralls-url]: https://coveralls.io/r/intech/moleculer-elastic-apm
+[downloads-image]: https://img.shields.io/npm/dm/moleculer-elastic-apm.svg
+[downloads-url]: https://npmjs.org/package/moleculer-elastic-apm
---
+++ index.js
@@ -0,0 +1,87 @@
+`use strict`;
+
+const apm = require('elastic-apm-node');
+
+/**
+ * Service metric traces to the Elastic APM.
+ *
+ * @name moleculer-elastic-apm
+ * @module Service
+ */
+module.exports = {
+
+  name: "elastic-apm",
+
+  /**
+   * Default settings for APM
+   */
+  settings: {
+    serviceName: 'Moleculer',
+    serverUrl: 'http://localhost:8200',
+    captureBody: 'all',
+    errorOnAbortedRequests: true
+  },
+
+  /**
+   * Events
+   */
+  events: {
+
+    "metrics.trace.span.start"(payload) {
+      this.requests[payload.id] = payload;
+      this.spans[payload.id] = apm.startSpan(this.getSpanName(payload), 'broker');
+
+      // Если не вложенный запрос, создаём транзакцию
+      if (!payload.parent) {
+        this.transactions[payload.id] = apm.startTransaction(this.getSpanName(payload), this.getType(payload));
+      }
+    },
+
+    "metrics.trace.span.finish"(payload) {
+      // Закрываем span
+      if (this.spans[payload.id]) {
+        let item = this.requests[payload.id];
+        Object.assign(item, payload);
+        if (item.meta.hasOwnProperty('apiKey')) apm.setTag('apiKey', item.meta.apiKey);
+        this.spans[payload.id].end();
+      }
+
+      if (!payload.parent) apm.endTransaction();
+      delete this.requests[payload.id];
+    }
+  },
+
+  /**
+   * Methods
+   */
+  methods: {
+
+    /**
+     * Get span name from metric event. By default it returns the action name
+     *
+     * @param {Object} metric
+     * @returns  {String}
+     */
+    getSpanName(metric) {
+      if (metric.name) return metric.name;
+      if (metric.action) return metric.action.name;
+      return 'unnamed';
+    },
+
+    getType(span) {
+      let type = 'request';
+      if (span.fromCache) type += ".cache";
+      if (span.remoteCall) type += ".remote";
+      if (span.error) type = ".error";
+      return type;
+    }
+  },
+
+  /**
+   * Service created lifecycle event handler
+   */
+  created() {
+    this.requests = {};
+    this.spans = {};
+  }
+};
